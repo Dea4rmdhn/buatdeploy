d33abb88c115e24fe162650ea82fdb23a2929d2f
Dea4rmdhn <deanandaramadhani12@gmail.com>
2025-02-23 08:28:02 +0700

    ulpoad git

---
+++ src/config.js
@@ -0,0 +1,16 @@
+const config = {
+    recapTitle: "Recap",
+    recapSections: [
+      { label: "Message", path: "/letter", icon: "Message" },
+      { label: "Image", path: "/recap/pictures", icon: "Image" },
+      { label: "Music", path: "/recap/music", icon: "Music" },
+    ],
+    recapPreviousPage: "/question",
+    recapNextPage: "/timer",
+    clickMeText: "Click Me",
+    previousPageText: "Back",
+    nextPageText: "Next",
+  };
+  
+  export default config;
+  
