436b1a95b2c66c95124e63466410051b1773c170
Dea4rmdhn <deanandaramadhani12@gmail.com>
2025-02-23 17:43:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/relationship_website_mobile---new/',
-
-  server: {
-    host: 'localhost', // This restricts the server to be accessible only from the local machine
-  },
   plugins: [react()],
-});
+})
