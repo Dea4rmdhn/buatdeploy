5427b5859d08cb2e92f9565a1e28d063b3113499
Dea4rmdhn <deanandaramadhani12@gmail.com>
2025-02-24 11:42:04 +0700

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,15 +1,24 @@
 {
     "version": 2,
     "builds": [
-      {
-        "src": "src/index.js",
-        "use": "@vercel/static-build"
-      }
+        {
+            "src": "src/index.js",
+            "use": "@vercel/static-build"
+        }
     ],
     "routes": [
-      {
-        "src": "/assets/(.*)",
-        "dest": "src/assets/$1"
-      }
-    ]
-  }
+        {
+            "src": "/assets/(.*)",
+            "dest": "src/assets/$1",
+            "headers": {
+                "Cache-Control": "public, max-age=31536000, immutable",
+                "Content-Type": {
+                    ".mp3": "audio/mpeg"
+                }
+            }
+        }
+    ],
+    "mimeTypes": {
+        ".mp3": "audio/mpeg"
+    }
+}
