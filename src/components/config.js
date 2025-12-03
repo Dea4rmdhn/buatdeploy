748aa82e9f0212372e02b9b7e981517e05125c1b
Dea4rmdhn <deanandaramadhani12@gmail.com>
2025-12-04 04:03:48 +0700

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -139,8 +139,8 @@ const config = {
       { title: "Desember 28, 2024", description : "Tahun baru-an bareng keluarga. soo exciteddd. semoga tahun depan bisa kaya gini lagii" },
       { title: "Januari 31, 2025", description: "ke alun-alun untuk ketiga kalinyaaa. Makasih bunganyaa ❤️" },
       { title: "Februari 1, 2025", description: " foto bareng mbah kong yeayyyy...h-1 amas balik. Jujur sedih pas ditinggal tapi gapapa kita ketemu lagi secepatnya ya sayanggg. Thank you for always being there for me. I appreciate you more than words can say ❤️" },
-      { title: "Desember 1, 2025", description: "baru" },
-      { title: "Desember 1, 2025", description: "baru sarangan" },
+      { title: "Oktober 18, 2025", description: "Jalan-jalan ke Sarangan, Melihat bunga di tepi dermaga. Walau jarak memisahkan badan, Cinta kita tetaplah terjaga.  " },
+      { title: "Desember 1, 2025", description: "kok disini ganteng yo. hmm" },
     ],
 
   // 💌 Love Letter Page
@@ -177,6 +177,7 @@ const config = {
 };
 
 export default config;
+
 
 
 
